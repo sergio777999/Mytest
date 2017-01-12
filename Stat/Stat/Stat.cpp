d20ac8a75c8a52ad861a554b02f6a3f38ff26715
Daniel Andrew <daniel.sa1@aol.com>
2017-01-11 22:14:07 -0500

    Stat_Test

---
+++ Stat/Stat/Stat.cpp
@@ -0,0 +1,26 @@
+#include <sys/stat.h>
+#include<iostream>
+#include<string>
+
+using namespace std;
+/**
+* Get the size of a file.
+* @param filename The name of the file to check size for
+* @return The filesize, or 0 if the file does not exist.
+*/
+size_t getFilesize(const std::string& filename) {
+	struct stat st;
+	if (stat(filename.c_str(), &st) != 0) {
+		return 0;
+	}
+	return st.st_size;
+}
+
+int main()
+{
+	const string filename{ R"(C:\Users\Daniel\Source\Repos\Mytest\stat.txt)" };
+	size_t size = getFilesize(filename);
+	cout << "The file size is: " << size << endl;
+
+	return 0;
+}
