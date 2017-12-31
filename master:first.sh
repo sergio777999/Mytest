6b97038c73021a8a919259e78a531987551fd791
sergio777999 <sergeslamat@gmail.com>
2017-12-31 17:58:14 -0500

    final test commit

---
+++ master:first.sh
@@ -0,0 +1,7 @@
+No <tool> given for --tool=<tool>
+usage: git difftool [-t|--tool=<tool>] [--tool-help]
+                    [-x|--extcmd=<cmd>]
+                    [-g|--gui] [--no-gui]
+                    [--prompt] [-y|--no-prompt]
+                    [-d|--dir-diff]
+                    ['git diff' options]
